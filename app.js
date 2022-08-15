cbd78166301271388bdd4a45aec4d1434091b966
BOUTOKPO Emmanuel <tossedeemmanuelboutokpo@gmail.com>
2022-08-15 13:07:53 +0100

    Exercice 4

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
- let note1 =parseFloat(prompt("Entrer votre note 1"))
- let note2 =parseFloat(prompt("Entrer votre note 2"))
- let note3 =parseFloat(prompt("Entrer votre note 3"))
- let coef1 =parseFloat(prompt("Entrer votre coef 1"))
- let coef2 =parseFloat(prompt("Entrer votre coef 2"))
- let coef3 =parseFloat(prompt("Entrer votre coef 3"))
- let moy, mention;
-
- if ((note1>0 && note1<=20) && (note2>0 && note2<=20)&&(note3>0 && note3<=20)) {
-    moy=(note1*coef1+note2*coef2 + note3*coef3)/(coef1+coef2+coef3);
-    if (moy<10) {
-        mention ="Insuffisant"
-    } else  if (moy>=10 && moy<12) {
-        mention="Passable"
-    }else if(moy>=12 && moy<14){
-        mention="Assez-bien"
-    }else if(moy>=14 && moy <16){
-        mention="Bien"
-    }else{
-        mention ="Très-bien"
-    }
- } else {
-    alert("Veuillez entrer un nombre entre 0 et 20")
- }
- alert("Votre moyenne : " + moy + " Mention : " + mention)
+ let a =parseInt(prompt("Entrer votre a"));
+ let b =parseInt(prompt("Entrer votre b"));
+ let c =parseInt(prompt("Entrer votre c"));
+ let discriminant, x1,x2, rep;
+alert("L'équation est : "+ a+"x²+"+b+"x+"+c);
+discriminant=b*b -4*(a*c);
+if (discriminant>0) {
+    x1=(-b+Math.sqrt(discriminant))/2*a;
+    x2=(-b-Math.sqrt(discriminant))/2*a;
+    rep= discriminant + " est positif alors l'équation admet deux solutions x1 et x2 respectivement " + x1+"et"+x2;
+} else if (discriminant==0){
+    x1=x2=-b/2*a;
+    rep= discriminant + " est nul alors l'équation  admet une racine double x1=x2" +x1;
+}else{
+    rep= discriminant + " est strictement négatif, l'équation n'admet pas de solution réelle, mais admet deux solutions complexes"
+}
+alert(rep)
